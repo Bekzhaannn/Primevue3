ea7fad07944fbe434b9044215ae97190738b0ffa
krivalex <new.krivalex@gmail.com>
2023-10-14 09:57:12 +0600

    in

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from '@/components/layouts/NavBar.vue'
+import LearnPrimeVue from './components/LearnPrimeVue.vue'
 </script>
 
 <template>
   <NavBar />
+  <LearnPrimeVue />
   <router-view />
 </template>
 
---
+++ src/components/LearnPrimeVue.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card">
+    <PDataTable :value="data">
+      <PColumn v-for="column in columns" :key="column.field" :field="column.field" :header="column.header" />
+    </PDataTable>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import PDataTable from 'primevue/datatable'
+import PColumn from 'primevue/column'
+
+const data = [
+  {
+    id: 1,
+    name: 'BMW',
+    price: 10000,
+  },
+  {
+    id: 2,
+    name: 'Audi',
+    price: 12000,
+  },
+  {
+    id: 3,
+    name: 'Mercedes',
+    price: 15000,
+  },
+]
+
+const columns = [
+  {
+    field: 'id',
+    header: 'ID',
+  },
+  {
+    field: 'name',
+    header: 'Название',
+  },
+  {
+    field: 'price',
+    header: 'Цена',
+  },
+]
+</script>
